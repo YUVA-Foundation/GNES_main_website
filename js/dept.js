c19f0ad743c51eb044d9b26339bff3a180d545fb
tejasimangale <tejasimangale@gmail.com>
2021-10-06 19:10:56 +0530

    specific dept

--- js/dept.js
+++ js/dept.js
@@ -10,9 +10,30 @@ const responsive={
     }
 }
 
-$('.faculty-card').owlCarousel({
+$('.achm').owlCarousel({
     loop:true,
+    nav:true,
     autoplay:true,
     autoplayTimeout:3000,
     responsive:responsive
+});
+
+const responsives={
+    1040:{
+        items:3
+    },
+    640:{
+        items:2
+    },
+    0:{
+        items:1
+    }
+}
+
+$('.faculty').owlCarousel({
+    loop:true,
+    margin:100,
+    autoplay:true,
+    autoplayTimeout:3000,
+    responsive:responsives
 });
